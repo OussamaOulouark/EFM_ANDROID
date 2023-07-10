8672c44b72c7192419624c4a7af7cae31665b627
OUSSAMA OULOUARK <alioulouark@gmail.com>
2023-07-10 09:11:31 +0100

    Task 01

--- app/src/main/java/com/example/efm/MainActivity.java
+++ app/src/main/java/com/example/efm/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.efm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ETnumber;
+    Spinner Option;
+    Button btncalcule;
 
+    @SuppressLint({"WrongViewCast", "CutPasteId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+     ETnumber = findViewById(R.id.ETnumber);
+     Option = findViewById(R.id.Option);
+     btncalcule = findViewById(R.id.btncalcule);
+
+
+
     }
 }
